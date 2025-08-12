63bc282d2d3d961e40d3af3bf8d9279f300113e2
64/sixtyfour64 <jamieleetaggart643@gmail.com>
2025-08-12 13:42:57 -0400

    thing

---
+++ Zenaxide/threads.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "zenaxide.h"
+
+VOID
+WINAPI
+TimerThread(
+	VOID
+);
---
+++ Zenaxide/zenaxide.h
@@ -0,0 +1,31 @@
+#pragma once
+#pragma warning( disable: 4152 )
+#pragma warning( disable: 4201 )
+
+#define RGBQUAD _RGBQUAD
+#pragma warning( push, 0 )
+#include <windows.h>
+#pragma warning( pop )
+#undef RGBQUAD
+
+#include <accctrl.h>
+#include <aclapi.h>
+#include <stdio.h>
+#include <math.h>
+#include "def.h"
+#include "math.h"
+#include "color.h"
+#include "callbacks.h"
+#include "threads.h"
+#include "payloads.h"
+#include "resource.h"
+#include "extra_payloads.h"
+#include "destruction.h"
+
+#pragma region Public Variables
+extern HWND hwndDesktop;
+extern HDC hdcDesktop;
+extern RECT rcScrBounds;
+extern HHOOK hMsgHook;
+extern INT nCounter;
+#pragma endregion Public Variables
